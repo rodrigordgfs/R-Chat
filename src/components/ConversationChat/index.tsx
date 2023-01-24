1b28b9e49f72d19dd602cdbf42bafefc600db46b
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-01-24 06:10:29 -0300

    Build Test

--- src/components/ConversationChat/index.tsx
+++ src/components/ConversationChat/index.tsx
@@ -2,7 +2,7 @@ import { ConversatinTileReceived } from "../ConversatinTileReceived";
 import { ConversationTileSent } from "../ConversationTileSent";
 
 export function ConversationChat() {
-  return (
+  return ( 
     <div className="flex-1 px-4 overflow-scroll bg-zinc-800 overflow-x-hidden scrollbar-thin scrollbar-thumb-zinc-800 scrollbar-track-zinc-600">
       <ConversatinTileReceived message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat sed lectus vestibulum mattis ullamcorper." />
       <ConversationTileSent message="Pharetra et ultrices neque ornare. Posuere morbi leo urna molestie at elementum eu facilisis. Egestas congue quisque egestas diam in arcu cursus." />
